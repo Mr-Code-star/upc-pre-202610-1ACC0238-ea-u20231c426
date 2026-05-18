f81e0c653fe26d58a6139e7b4c2e2c67f653924c
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-18 11:15:46 -0500

    Hize una prueba unitaria si todo la configuracion que hize funciona cosas que si todo esta bien.

--- app/src/main/java/com/example/upc_pre_202610_1acc0238_ea_u20231c426/MainActivity.kt
+++ app/src/main/java/com/example/upc_pre_202610_1acc0238_ea_u20231c426/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import com.example.upc_pre_202610_1acc0238_ea_u20231c426.presentation.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,8 +21,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                //
+                // Hacemos prueba si funcion todo bien cosa que si todo esta funcionando correcto
+                // con las dependencias y configuraciones
+                // Bueno Vitaly del futuro buen examen (Tranquilo, no te extreses todo saldra bien)
+                Main()
             }
         }
+    }
+}
+
+/**
+ * Es un Main de Hello Wordl este main no va ser importante en el examen
+ */
+@Composable
+fun Main() {
+    Box(
+        modifier = Modifier.fillMaxSize(), // Ocupa toda la pantalla
+        contentAlignment = Alignment.Center // Centra el contenido
+    ) {
+        Text(
+            text = "Hello World",
+            fontSize = 24.sp
+        )
     }
 }
